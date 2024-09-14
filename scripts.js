67644636c2f1541a5b3827634b210a9650ac0dc7
pedr0martss <121310953+pedr0martss@users.noreply.github.com>
2024-09-14 19:19:34 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,7 @@
-// Detecta o movimento do mouse
-document.addEventListener('mousemove', (event) => {
-    // Obtém as coordenadas do mouse
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-
-    // Obtém a largura e altura da janela
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    // Calcula a porcentagem da posição do mouse
-    const xPercent = (mouseX / width) * 100;
-    const yPercent = (mouseY / height) * 100;
-
-    // Aplica a distorção ao fundo do body
-    document.body.style.backgroundPosition = `${xPercent}% ${yPercent}%`;
-});
-
-document.addEventListener('mousemove', (event) => {
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    // Calcular valores de distorção com base na posição do mouse
-    const xPercent = (mouseX / width) * 100;
-    const yPercent = (mouseY / height) * 100;
-
-    document.body.style.backgroundPosition = `${xPercent}% ${yPercent}%`;
+document.addEventListener('mousemove', (e) => {
+  const body = document.querySelector('body');
+  const x = e.clientX / window.innerWidth;
+  const y = e.clientY / window.innerHeight;
+  body.style.setProperty('--mouse-x', x);
+  body.style.setProperty('--mouse-y', y);
 });
